8542b0eb7cc88c8a60ceca73841c0bd03066fa77
unknown <ministrov2018@gmail.com>
2024-01-29 18:50:41 +0300

    feat: end up operating on form checkbox and radio button decorating

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,9 +5,7 @@ const navMain = document.querySelector('.site-navigation');
 const navToggle = document.querySelector('.site-navigation__toggle');
 
 navToggle.addEventListener('click', () => {
-  console.log('clicked');
   if (navMain.classList.contains('site-navigation--closed')) {
-    console.log('clicked');
     navMain.classList.remove('site-navigation--closed');
     navMain.classList.add('site-navigation--opened');
   } else {
@@ -15,7 +13,3 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('site-navigation--opened');
   }
 });
-
-// const a = 'my sleep';
-
-// console.log(a);
